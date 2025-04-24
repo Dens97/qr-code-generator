1997de9d98a26595513662c4270287975acc0cfd
Dens97 <lessing.dennis@gmail.com>
2025-04-24 10:57:08 +0200

    Fix Policy problems and create save tab-button

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const inputEl = document.getElementById("input-el");
 const generateBtn = document.getElementById("generate-btn");
 const qrEl = document.getElementById("qr-el");
+const currentBtn = document.getElementById("current-btn");
 
 generateBtn.addEventListener("click", function () {
   if (inputEl.value) {
-    qrEl.innerHTML = "";
-    const qrcode = new QRCode(qrEl, inputEl.value);
+    generateQR(inputEl.value);
   }
 });
+
+currentBtn.addEventListener("click", function () {
+  //trigger QR Code Function but input custom tab instead of input val
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log(typeof tabs[0].url);
+    generateQR(tabs[0].url);
+  });
+});
+
+function generateQR(url) {
+  qrEl.innerHTML = "";
+  new QRCode(qrEl, url);
+}
